60a48566ab5b418d3d56c1ad0b12aba7238f19b3
Stillalivepc <Stillalivepc@users.noreply.github.com>
2023-12-21 16:16:19 +0000

    deploy: 6a2b0047f4daa9395d74a1d09f5880863cc20335

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -6,11 +6,19 @@
 // YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre>                     __                             ___       __  
-   ____  ____  _____/ /__________  ____ ___  ____  / (_)___  / /__
-  / __ \\/ __ \\/ ___/ __/ ___/ __ \\/ __ '__ \\/ __ \\/ / / __ \\/ //_/
- / / / / /_/ (__  ) /_/ /  / /_/ / / / / / / /_/ / / / / / / ,&lt;   
-/_/ /_/\\____/____/\\__/_/   \\____/_/ /_/ /_/\\____/_/_/_/ /_/_/|_|  
-                                                                 </pre> </div>
+<div class="custom-text-header"> <pre>                                                                               :                          
+                                                 .,G:                          t#,                         
+  .                      i                      ,WtE#,    :       .           ;##W.             i          
+  Ef.                   LE              ..     i#D.E#t  .GE       Ef.        :#L:WE            LE GEEEEEEEL
+  E#Wi                 L#E             ;W,    f#f  E#t j#K;       E#Wi      .KG  ,#D          L#E ,;;L#K;;.
+  E#K#D:              G#W.            j##,  .D#i   E#GK#f         E#K#D:    EE    ;#f        G#W.    t#E   
+  E#t,E#f.           D#K.            G###, :KW,    E##D.          E#t,E#f. f#.     t#i      D#K.     t#E   
+  E#WEE##Wt         E#K.           :E####, t#f     E##Wi          E#WEE##Wt:#G     GK      E#K.      t#E   
+  E##Ei;;;;.      .E#E.           ;W#DG##,  ;#G    E#jL#D:        E##Ei;;;;.;#L   LW.    .E#E.       t#E   
+  E#DWWt         .K#E            j###DW##,   :KE.  E#t ,K#j       E#DWWt     t#f f#:    .K#E         t#E   
+  E#t f#K;      .K#D            G##i,,G##,    .DW: E#t   jD       E#t f#K;    f#D#;    .K#D          t#E   
+  E#Dfff##E,   .W#G           :K#K:   L##,      L#,j#t            E#Dfff##E,   G#t    .W#G           t#E   
+  jLLLLLLLLL; :W##########Wt ;##D.    L##,       jt ,;            jLLLLLLLLL;   t    :W##########Wt   fE   
+              :,,,,,,,,,,,,,.,,,      .,,                                            :,,,,,,,,,,,,,.   :   </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
