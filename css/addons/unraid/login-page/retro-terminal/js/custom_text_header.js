e39b73798443594a1a2b07ec8006b22defd9a7f3
Stillalivepc <53355028+Stillalivepc@users.noreply.github.com>
2023-12-21 11:25:05 -0500

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -6,19 +6,14 @@
 // YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre>                                                                               :                          
-                                                 .,G:                          t#,                         
-  .                      i                      ,WtE#,    :       .           ;##W.             i          
-  Ef.                   LE              ..     i#D.E#t  .GE       Ef.        :#L:WE            LE GEEEEEEEL
-  E#Wi                 L#E             ;W,    f#f  E#t j#K;       E#Wi      .KG  ,#D          L#E ,;;L#K;;.
-  E#K#D:              G#W.            j##,  .D#i   E#GK#f         E#K#D:    EE    ;#f        G#W.    t#E   
-  E#t,E#f.           D#K.            G###, :KW,    E##D.          E#t,E#f. f#.     t#i      D#K.     t#E   
-  E#WEE##Wt         E#K.           :E####, t#f     E##Wi          E#WEE##Wt:#G     GK      E#K.      t#E   
-  E##Ei;;;;.      .E#E.           ;W#DG##,  ;#G    E#jL#D:        E##Ei;;;;.;#L   LW.    .E#E.       t#E   
-  E#DWWt         .K#E            j###DW##,   :KE.  E#t ,K#j       E#DWWt     t#f f#:    .K#E         t#E   
-  E#t f#K;      .K#D            G##i,,G##,    .DW: E#t   jD       E#t f#K;    f#D#;    .K#D          t#E   
-  E#Dfff##E,   .W#G           :K#K:   L##,      L#,j#t            E#Dfff##E,   G#t    .W#G           t#E   
-  jLLLLLLLLL; :W##########Wt ;##D.    L##,       jt ,;            jLLLLLLLLL;   t    :W##########Wt   fE   
-              :,,,,,,,,,,,,,.,,,      .,,                                            :,,,,,,,,,,,,,.   :   </pre> </div>
+<div class="custom-text-header"> <pre> ______   _        _______  _______  _          ______   _______  _    _________
+(  ___ \ ( \      (  ___  )(  ____ \| \    /\  (  ___ \ (  ___  )( \   \__   __/
+| (   ) )| (      | (   ) || (    \/|  \  / /  | (   ) )| (   ) || (      ) (   
+| (__/ / | |      | (___) || |      |  (_/ /   | (__/ / | |   | || |      | |   
+|  __ (  | |      |  ___  || |      |   _ (    |  __ (  | |   | || |      | |   
+| (  \ \ | |      | (   ) || |      |  ( \ \   | (  \ \ | |   | || |      | |   
+| )___) )| (____/\| )   ( || (____/\|  /  \ \  | )___) )| (___) || (____/\| |   
+|/ \___/ (_______/|/     \|(_______/|_/    \/  |/ \___/ (_______)(_______/)_(   
+                                                                               </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
